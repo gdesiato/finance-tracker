8912bc398bef5e1073146a7d54d75359c73957ff
Giuseppe Desiato <giodesiato@gmail.com>
2024-04-17 22:05:04 -0400

    user data class

---
+++ src/main/kotlin/com/gdesiato/financetracker/model/User.kt
@@ -0,0 +1,19 @@
+package com.gdesiato.financetracker.model
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "users")
+data class User(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(nullable = false)
+    val email: String,
+
+    @Column(nullable = false)
+    val password: String,
+    
+)
